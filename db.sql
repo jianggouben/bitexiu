70844d1012467df0848b567bb371f8849d0737c2
Haulyn Jason <iam@saharabear.com>
2014-07-08 09:29:41 +0800

    init project bitexiu

---
+++ db.sql
@@ -0,0 +1,13 @@
+create user bitexiu with password 'bitexiu' ;
+
+ALTER USER bitexiu WITH PASSWORD 'bitexiu';
+
+create database bitexiu with encoding='utf8' ;
+
+grant all privileges on database bitexiu to bitexiu ;
+
+\connect bitexiu;
+
+alter database bitexiu owner to bitexiu;
+
+alter schema public owner to bitexiu;

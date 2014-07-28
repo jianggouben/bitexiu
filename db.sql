0a316febdfcf864d30475e8ec2d2f5a394522c18
Haulyn Jason <iam@saharabear.com>
2014-07-29 07:22:29 +0800

    upgrade to new version of symfony

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
-create user bitexiu with password 'bitexiu';
+create user bitexiu with password 'bitexiu123';
 
-ALTER USER bitexiu WITH PASSWORD 'bitexiu';
+ALTER USER bitexiu WITH PASSWORD 'bitexiu123';
 
 create database bitexiu with encoding='utf8';
 
